1d2b12e4aa7fac5f067a3758971481c279176f6b
jzhong0821 <jzhong0821@gmail.com>
2023-10-16 15:34:13 -0700

    title stuff

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://www.rlshistory.club/", // replace this with your deployed domain
+  author: "Jack Zhong",
+  desc: "The Pirate Journal is a student-led publications ran by Rls History Club",
+  title: "The Pirate Journal - A student-led publication",
+  ogImage: "/assets/og.png",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
