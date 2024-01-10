3abaf7130e5ef8406b0d8627ba5e102aa376ee4c
jzhong0821 <jzhong0821@gmail.com>
2024-01-09 21:43:59 -0800

    aaaq

--- astro.config.ts
+++ astro.config.ts
@@ -5,17 +5,11 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
